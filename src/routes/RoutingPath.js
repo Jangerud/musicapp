0da07d1a6c147970ca0a3c94e112a07736cce696
jange <jange@DESKTOP-1PGUA82>
2021-09-09 12:11:35 +0200

    Add different views and RoutingPath.js content.

---
+++ src/routes/RoutingPath.js
@@ -0,0 +1,13 @@
+const signUpView = "/signup";
+const homeView = "/home";
+const loginView = "/login";
+const profileView = "/profile";
+const usersView = "/users";
+
+export default {
+    homeView,
+    loginView,
+    profileView,
+    signUpView,
+    usersView,
+}
