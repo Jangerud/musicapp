d129901d3d90a1bba52e2cac541c53ee848515c1
jange <jange@DESKTOP-1PGUA82>
2021-09-09 09:51:24 +0200

    Add logo for website on HomeView, add style for the HomeView.css.

---
+++ src/components/WelcomeMessage/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import img from '../../shared/images/MusicBuddy.png'
+
+export const Welcome = () => {
+
+    return (
+        <div>
+            <img src={img} alt="A logo"></img>
+        </div>
+    )
+}
