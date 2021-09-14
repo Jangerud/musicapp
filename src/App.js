014b8f52fcd0347792681d7b1619e2404067a2c6
jange <jange@DESKTOP-1PGUA82>
2021-09-14 10:37:50 +0200

    Added Axios for future use. Added UserProvider under shared to the project. Set up the use of UserProvider aswell in order to build a login function for the app.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { Router } from "react-router";
 import { HomeView } from './view/homeView/HomeView'
 import {Routes} from './routes/Routes'
+import { UserProvider } from "./shared/provider/UserProvider";
 
 function App() {
   return (
-    <div>
-      <Routes />
-    </div>
+    <UserProvider>
+        <Routes>
+          
+        </Routes>
+    </UserProvider>
   );
 }
 
--- src/view/loginView/LoginView.js
+++ src/view/loginView/LoginView.js
@@ -2,11 +2,13 @@ import React from 'react'
 import { Welcome } from '../../components/WelcomeMessage/Welcome'
 import './LoginView.css'
 import {Link} from 'react-router-dom'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
+import { UserContext } from '../../shared/provider/UserProvider'
 
 export const LoginView = () => {
     const [username, setUsername] = useState("Johan")
     const [password, setPassword] = useState();
+    const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
 
     return (
         <div>
@@ -17,7 +19,7 @@ export const LoginView = () => {
                 <main className="bottom">
                     <section className="message">
                     <h1>Login</h1>
-                    <p>Username:</p> <input placeholder="Enter username" onChange={(event) => setUsername(event.target.value)}></input> <br />
+                    <p>Username: {username}</p> <input placeholder="Enter username" onChange={(event) => setUsername(event.target.value)}></input> <br />
                     <p>Password:</p> <input placeholder="Enter password" onChange={(event) => setPassword(event.target.value)}></input> <br />
                     
                     </section>
