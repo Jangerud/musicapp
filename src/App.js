ecb49f82405ebe99d5ddaa4124f3f069ddbbf86f
jange <jange@DESKTOP-1PGUA82>
2021-09-15 09:54:16 +0200

    Added Navigation components for desktop and mobile. Also added Navigation as a child to Routes in App.js.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { Router } from "react-router";
 import { HomeView } from './view/homeView/HomeView'
 import {Routes} from './routes/Routes'
 import { UserProvider } from "./shared/provider/UserProvider";
+import { Navigation } from "./components/navigation/Navigation";
 
 function App() {
   return (
     <UserProvider>
         <Routes>
-          
+          <Navigation />
         </Routes>
     </UserProvider>
   );
---
+++ src/components/navigation/DesktopNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const DesktopNavigation = () => {
+    return (
+        <div>
+            <span>Home</span>
+            <span>Login</span>
+            <span>Sign Up</span>
+        </div>
+    )
+}
