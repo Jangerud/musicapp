22495da0891ddbab493e4a4f8d4acdcc24ee3c93
jange <jange@DESKTOP-1PGUA82>
2021-09-16 10:19:39 +0200

    Add structure to the DesktopNavigation navbar aswell as styling from the corresponding css file.

--- src/components/navigation/DesktopNavigation/DesktopNavigation.js
+++ src/components/navigation/DesktopNavigation/DesktopNavigation.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import RoutingPath from '../../../routes/RoutingPath'
+import img from '../../../shared/images/MB-logo.png'
+import '../DesktopNavigation/DesktopNavigation.css'
 
 export const DesktopNavigation = () => {
     return (
         <div className="deskDiv">
-            <section className="navbar">
-            <Link to={RoutingPath.homeView}>Home</Link>
-            <Link to ={RoutingPath.loginView}>Login</Link>
-            <Link to={RoutingPath.signUpView}>Sign Up</Link>
-            </section>
+            <nav className="deskNavbar" >
+                <img src={img} className="navbar__logo" alt="Navbar logo." />
+                <ul className="navbar__list--desktop">
+                    <Link to={RoutingPath.homeView}>Home</Link>
+                    <Link to ={RoutingPath.loginView}>Login</Link>
+                    <Link to={RoutingPath.signUpView}>Sign Up</Link>
+                </ul>
+
+            </nav>
         </div>
     )
 }
