5b86c9d4c5e3e29df46399c1dd35442e3fa83f63
jange <jange@DESKTOP-1PGUA82>
2021-09-16 15:35:44 +0200

    Added style to the navbar. Also added a way for the login link in the navbar to change to the logged in users name when authenticated.

--- src/components/navigation/DesktopNavigation/DesktopNavigation.js
+++ src/components/navigation/DesktopNavigation/DesktopNavigation.js
@@ -3,16 +3,30 @@ import { Link } from 'react-router-dom'
 import RoutingPath from '../../../routes/RoutingPath'
 import img from '../../../shared/images/MB-logo.png'
 import '../DesktopNavigation/DesktopNavigation.css'
+import { useContext } from 'react'
+import { UserContext } from '../../../shared/provider/UserProvider.js'
 
 export const DesktopNavigation = () => {
+    const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
+    const renderLogin = () => {
+        if (authenticatedUser) return <span>{authenticatedUser}</span>;
+        return (
+            <Link to ={RoutingPath.loginView}>Login</Link>
+        );
+           
+        
+    }
+    
+
     return (
         <div className="deskDiv">
             <nav className="deskNavbar" >
                 <img src={img} className="navbar__logo" alt="Navbar logo." />
                 <ul className="navbar__list--desktop">
                     <Link to={RoutingPath.homeView}>Home</Link>
-                    <Link to ={RoutingPath.loginView}>Login</Link>
-                    <Link to={RoutingPath.signUpView}>Sign Up</Link>
+                    <Link to={RoutingPath.usersView}>Users</Link>
+                    <Link to={RoutingPath.profileView}>Profile</Link>
+                    {renderLogin()}
                 </ul>
 
             </nav>
--- src/view/loginView/LoginView.js
+++ src/view/loginView/LoginView.js
@@ -3,13 +3,17 @@ import { Welcome } from '../../components/WelcomeMessage/Welcome'
 import './LoginView.css'
 import {Link} from 'react-router-dom'
 import { useState, useContext } from 'react'
-import { UserContext } from '../../shared/provider/UserProvider'
 import RoutingPath from '../../routes/RoutingPath'
+import { UserContext } from '../../shared/provider/UserProvider'
 
 export const LoginView = () => {
-    const [username, setUsername] = useState("Johan")
+    const [username, setUsername] = useState("Johan");
     const [password, setPassword] = useState();
     const [authenticatedUser, setAuthenticatedUser] = useContext(UserContext);
+    
+    const login = () => {
+        setAuthenticatedUser(username)
+    };
 
     return (
         <div className="login">
@@ -19,14 +23,15 @@ export const LoginView = () => {
 
                 <main className="bottom">
                     
-                    <h1>Login</h1>
-                    <p className="p1">Username: {username}</p> <input className="usernameInput" placeholder="Enter username" onChange={(event) => setUsername(event.target.value)}></input>
-                    <p className="p2">Password:</p> <input className="passwordInput" placeholder="Enter password" onChange={(event) => setPassword(event.target.value)}></input>
+                    <p className="p1">Username: {username}</p> 
+                    <input className="usernameInput" placeholder="Enter username" onChange={(event) => setUsername(event.target.value)}></input>
+                    <p className="p2">Password:</p> 
+                    <input className="passwordInput" placeholder="Enter password" onChange={(event) => setPassword(event.target.value)}></input>
                     
                    
 
                     <section className="buttons">
-                    <Link to={RoutingPath.homeView} className="loginButton">Login</Link>
+                    <Link to={RoutingPath.homeView} className="loginButton" onClick={() => login()}>Login</Link>
                     </section>
 
                     
