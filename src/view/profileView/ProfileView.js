96eea8e6defd3ce9147bca3c965fbad9d5035a92
jange <jange@DESKTOP-1PGUA82>
2021-09-22 15:50:30 +0200

    Add an API to get a pokemon when button is clicked in the ProfileView. Also added API files in the api folder to facilitate this.

--- src/view/profileView/ProfileView.js
+++ src/view/profileView/ProfileView.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
 
 export const ProfileView = () => {
+    const [serverResponse, setServerResponse] = useState();
+
+    const fetchData = async () => {
+        const API_URL = "https://pokeapi.co/api/v2/pokemon/1";
+        try {
+            const response = await axios.get(API_URL);
+            setServerResponse(response);
+        }
+        catch (error) {
+            alert("Error recieving data from server: " + error);
+        }
+    };
+
     return (
-        <div>
-            <h1>Coming soon!</h1>
-        </div>
-    )
+        <main><section>
+            <h1>This is the profile view</h1>
+            <h2>{serverResponse?.data?.name}</h2>
+            <button onClick={() => fetchData()}>Get Avatar</button>
+            </section></main>
+    );
 }
